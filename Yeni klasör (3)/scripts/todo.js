a0eb6985d29a2dac247df0c8a7d69f2440cba47c
RINA-MOZZIE <83635736+RINA-MOZZIE@users.noreply.github.com>
2021-10-05 10:32:45 +0300

    Update todo.js

--- Yeni klasör (3)/scripts/todo.js
+++ Yeni klasör (3)/scripts/todo.js
@@ -1,9 +1,11 @@
+//tanımlama
 var wi = document.getElementById('wordinput');
 var butto = document.getElementById('add');
 var list = document.getElementById('liste');
 var element = document.getElementsByTagName('li');
-
+//butona bir tıklanma fonksiyonu atıyoruz
 butto.addEventListener('click', () => {
+ //eğer bir şey yazılmamışsa uyarı vermesini sağlıyoruz
 let word = wi.value;
 if(!word.length) return alert('Type anything in the text box!');
 let li = document.createElement('li');
@@ -11,7 +13,7 @@ li.innerText = word;
 list.insertBefore(li, list.childNodes[0]);
 wi.value = "";
 });
-
+//Li'deki bir yazıya tıklandığında renginin değişmesini ve üzerine çizik atılmasını sağlıyoruz
 list.addEventListener('click', (e) => {
  if(e.target.tagName === 'LI'){
      e.target.classList.toggle('complated');
